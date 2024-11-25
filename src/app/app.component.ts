88df1930fe7a6ee70cedbc9dee2994551c145413
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2024-11-25 15:15:32 +0100

    Basic foundation setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'SwayWebApp';
-}
+export class AppComponent {}
