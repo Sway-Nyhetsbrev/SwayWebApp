733df0893345c67df76fe42cfa20f0fc6726a18a
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2025-01-10 15:56:25 +0100

    Added styling on Header and Footer. adminPortal under development. allUsers, allNewsletters, searchUser and searchNewsletter component created.

---
+++ src/app/views/admin/admin-portal/all-newsletters/all-newsletters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-newsletters',
+  imports: [],
+  templateUrl: './all-newsletters.component.html',
+  styleUrl: './all-newsletters.component.scss'
+})
+export class AllNewslettersComponent {
+
+}
---
+++ src/app/views/admin/admin-portal/search-newsletter/search-newsletter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-newsletter',
+  imports: [],
+  templateUrl: './search-newsletter.component.html',
+  styleUrl: './search-newsletter.component.scss'
+})
+export class SearchNewsletterComponent {
+
+}
