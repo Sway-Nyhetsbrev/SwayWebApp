c0a122a00f1b1d01713911a3335cfb9806283d6d
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2025-03-31 17:42:57 +0200

    search component under development

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  searchTerm: string = '';
 
+  @Output() searchEvent = new EventEmitter<string>();
+
+  onSearch() {
+    this.searchEvent.emit(this.searchTerm);
+  }
 }
