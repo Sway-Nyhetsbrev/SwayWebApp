54bf5662609324e9da84c1c7f6d97c702726791b
NilsL <NilsL@MSI>
2025-04-01 17:29:34 +0200

    info tablet under development

--- src/app/views/info-tablet/info-tablet.component.ts
+++ src/app/views/info-tablet/info-tablet.component.ts
@@ -1,21 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NewsletterService } from '../../services/newsletter.service';
-import { DatePipe, NgStyle } from '@angular/common';
+import { DatePipe, NgClass, NgStyle } from '@angular/common';
 import { NewsletterSectionBase } from '../newsletter/create-newsletter/create-newsletter-section/newsletter-section-base';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-info-tablet',
-  imports: [FormsModule, NgStyle],
+  imports: [FormsModule, NgStyle, NgClass],
   templateUrl: './info-tablet.component.html',
   styleUrl: './info-tablet.component.scss'
 })
-export class InfoTabletComponent extends NewsletterSectionBase implements OnInit{
+export class InfoTabletComponent extends NewsletterSectionBase implements OnInit {
   activatedRoute = inject(ActivatedRoute);
   newsletterService = inject(NewsletterService);
   newsletterId = "";
-  sectionChecked = false;
+  selectedSections: boolean[] = [];
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -25,39 +25,40 @@ export class InfoTabletComponent extends NewsletterSectionBase implements OnInit
   }
 
   loadNewsletterDetails() {
-      const datePipe = new DatePipe('en-US');
-      const subscription = this.newsletterService.getOneNewsletter(this.newsletterId).subscribe({
-        next: (response) => {
-          console.log("newsletterDetails", response);
-          if (response.releaseDate) {
-            const formattedDate = datePipe.transform(response.releaseDate, 'yyyy-MM-dd');
-            if (formattedDate) {
-              response.releaseDate = formattedDate;
-            }
+    const datePipe = new DatePipe('en-US');
+    const subscription = this.newsletterService.getOneNewsletter(this.newsletterId).subscribe({
+      next: (response) => {
+        if (response.releaseDate) {
+          const formattedDate = datePipe.transform(response.releaseDate, 'yyyy-MM-dd');
+          if (formattedDate) {
+            response.releaseDate = formattedDate;
           }
-          this.newsletter.set(response);
-        },
-        error: (error) => {
-          this.statusMessage = 'Unable to load newsletter!';
-          this.statusClass = 'alert alert-danger';
-          console.log("Can't load newsletterdetails", error);
-        },
-      });
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
-      });
+        }
+        this.selectedSections = response.sections.map(() => false);
+        this.newsletter.set(response);
+      },
+      error: (error) => {
+        this.statusMessage = 'Unable to load newsletter!';
+        this.statusClass = 'alert alert-danger';
+        console.log("Can't load newsletter details", error);
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  toggleSection(index: number) {
+    this.selectedSections[index] = !this.selectedSections[index];
+    console.log("Section", index, "toggled:", this.selectedSections[index]);
   }
 
   async publishNewsletter() {
     if (this.newsletter()!.title && this.newsletter()!.releaseDate) {
-      // Pre-validate the sections before sending the update
       const sectionsValid = await this.validateSections();
-
       if (!sectionsValid) {
-        // If any section is too large, abort the update
         return;
       }
-
       try {
         const subscription = this.newsletterService.publishNewsletter(this.newsletter()).subscribe({
           next: (response) => {
@@ -77,10 +78,5 @@ export class InfoTabletComponent extends NewsletterSectionBase implements OnInit
         this.statusClass = 'alert alert-danger';
       }
     }
-  }
-
-  sectionsToSend() {
-    this.sectionChecked = !this.sectionChecked;
-    console.log("Checked section", this.sectionChecked);
   }
 }
